d237eb1a82030356d729e46d54ce3f4319086e7e
Nk-Drakula <emmyressure2@gmail.com>
2026-07-08 20:44:30 +0100

    Example: Synchronous File Read

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-import {getPosts} from "./postController.js"
+// import {getPosts} from "./postController.js"
 
-// const { generateRandomNumber, celsiusToFahrenheit } = require("./utils");
+// // const { generateRandomNumber, celsiusToFahrenheit } = require("./utils");
 
-// const randNum = generateRandomNumber();
+// // const randNum = generateRandomNumber();
 
-// console.log(`Random Number: ${generateRandomNumber()}`);
-// console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(randNum)}`);
+// // console.log(`Random Number: ${generateRandomNumber()}`);
+// // console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(randNum)}`);
 
-console.log(getPosts())
+// console.log(getPosts())
+
+
+const fs = require('fs');
+
+console.log('1. Starting sync read...');
+const data = fs.readFileSync('myfile.txt', 'utf8');
+console.log('2. File contents:', data);
+console.log('3. Done reading file');
